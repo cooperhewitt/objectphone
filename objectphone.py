e6a61c22fce5f34c52be356adc0af9e0d73ea0fa
Micah Walter <micah@micahwalter.com>
2014-02-13 10:17:25 -0500

    rewriting the sms part

--- objectphone.py
+++ objectphone.py
@@ -1,185 +1,144 @@
+import sys
 import os
 from flask import Flask, request, redirect
 from twilio import twiml
-import cStringIO
-import pycurl
-import urllib
-import simplejson as json
 
-api_token = os.environ['CH_API_KEY']
+import cooperhewitt.api.client
+
+token = os.environ['CH_API_KEY']
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def hello():
 	r = twiml.Response()
 	r.say("Welcome to object phone!. ") 
-	with r.gather(timeout=5, numDigits=1, action="initial-handler", method="POST") as g:
-		g.say("Press one on your touchtone phone to search the Cooper-Hewitt collection by object ID. ")
-		g.say("or, For a random object, press 2. ")
-		
-	r.say("I'm sorry, I missed that, please try again. ")
-	r.redirect(url="/", method="GET")	
-	return str(r)
-	
-@app.route('/initial-handler', methods=['GET','POST'])
-def handlecall():
-	digits = request.values.get('Digits', None)
-	
-	r = twiml.Response()
-	
-	if (digits == "1"):
-		with r.gather(timeout=10, action="object", method="POST") as g:
-			g.say("Please enter an object ID followed by the pound key.")
-		
-		r.say("I'm sorry, I missed that, please try again. ")
-		r.redirect(url="/initial-handler?Digits=1", method="POST", )
-		return str(r)
-	
-	if (digits == "2"):
-		r.redirect(url="/random", method="GET")
-		return str(r)
-	
-	r.say("I'm sorry, that choice is invalid, please try again. ")
-	r.redirect(url="/", method="GET")	
-	return str(r)
-		
-
-@app.route('/object', methods=['GET','POST'])
-def object():
-	obj_id = request.values.get('Digits', None)
-	r = twiml.Response()
-	
-	buf = cStringIO.StringIO()
-	c = pycurl.Curl()
-	c.setopt(c.URL, 'https://api.collection.cooperhewitt.org/rest')
-	d = {'method':'cooperhewitt.objects.getInfo','access_token':api_token, 'id':obj_id}
-	c.setopt(c.WRITEFUNCTION, buf.write)
-	c.setopt(c.POSTFIELDS, urllib.urlencode(d) )
-	c.perform()
-	
-	rsp_obj = json.loads(buf.getvalue())
-	buf.reset()
-	buf.truncate()
-	object_id = rsp_obj.get('object', [])
-	medium = object_id.get('medium', [])
-	title = object_id.get('title', [])
-	
-	phrase = "Hi you've reached  "
-
-	if (title):
-		phrase = phrase + title + ". "
-		
-	if (medium):
-		phrase = phrase + "My medium is " + medium + ". "
-			
-	r.say(phrase)
-	r.redirect(url="/end-menu", method="GET")
+	r.say("I'm sorry, but we are currently performing some maintenance. Please feel free to talk to us by SMS for the time being! ")
 	return str(r)
 
 
-@app.route('/random')
-def random():
-	buf = cStringIO.StringIO()
-	c = pycurl.Curl()
-	c.setopt(c.URL, 'https://api.collection.cooperhewitt.org/rest')
-	d = {'method':'cooperhewitt.objects.getRandom','access_token':api_token}
-	c.setopt(c.WRITEFUNCTION, buf.write)
-	c.setopt(c.POSTFIELDS, urllib.urlencode(d) )
-	c.perform()
-	
-	random = json.loads(buf.getvalue())
-	buf.reset()
-	buf.truncate()
-	object_id = random.get('object', [])
-	medium = object_id.get('medium', [])
-	title = object_id.get('title', [])
-	
-	phrase = "Thanks, we are looking up a random object just for you. "
-	if (title):
-		phrase = phrase + "Hi, you've reached " + title + ". "
-		
-	if (medium):
-		phrase =  phrase + "My medium is " + medium + ". "
-			
-	
-	r = twiml.Response()
-	r.say(phrase)
-	r.redirect(url="/end-menu", method="GET")
-	
-	return str(r)
-	
 @app.route('/sms', methods=['GET','POST'])
 def sms():
-	obj_id = request.values.get('Body', None)
-	r = twiml.Response()
-	
-	buf = cStringIO.StringIO()
-	c = pycurl.Curl()
-	c.setopt(c.URL, 'https://api.collection.cooperhewitt.org/rest')
-	
-	if (obj_id.lower() == "random"):
-		d = {'method':'cooperhewitt.objects.getRandom','access_token':api_token}
-	else:
-		d = {'method':'cooperhewitt.objects.getInfo','access_token':api_token, 'id':obj_id}
+    
+    body = request.values.get('Body', None)
+    
+    if (body):
+        sms_text = process_body(body)
+    else:
+        sms_text = sms_help()
+        
+    r = twiml.Response()
+    r.sms(sms_text)
+    return str(r)
 
-	c.setopt(c.WRITEFUNCTION, buf.write)
-	c.setopt(c.POSTFIELDS, urllib.urlencode(d) )
-	c.perform()
-	
-	rsp_obj = json.loads(buf.getvalue())
-	buf.reset()
-	buf.truncate()
-	object_id = rsp_obj.get('object', [])
-	id_value = object_id.get('id', [])
-	medium = object_id.get('medium', [])
-	title = object_id.get('title', [])
-	
-	phrase = "Thanks for texting me. "
+def wwms():
+    api = cooperhewitt.api.client.OAuth2(token)
+    rsp = api.call('cooperhewitt.labs.whatWouldMicahSay')
 
-	if (title):
+    micah = rsp['micah']
+    says = micah['says']
+
+    return says
+    
+def random():
+    api = cooperhewitt.api.client.OAuth2(token)
+    rsp = api.call('cooperhewitt.objects.getRandom')
+
+    random = rsp['object']
+    
+    return process_sms_object(random)
+
+def get_by_object_id(object_id):
+
+    api = cooperhewitt.api.client.OAuth2(token)
+    method = 'cooperhewitt.objects.getInfo'
+    args = { 'id': object_id }
+		
+    rsp = api.call(method, **args)
+
+    if (rsp['stat'] == 'ok'):
+        obj = rsp['object']
+        return process_sms_object(obj)
+    else:
+        return sms_oops()
+
+def get_by_accession_number(accession):
+
+    api = cooperhewitt.api.client.OAuth2(token)
+    method = 'cooperhewitt.objects.getInfo'
+    args = { 'accession_number': accession }
+		
+    rsp = api.call(method, **args)
+
+    if (rsp['stat'] == 'ok'):
+        obj = rsp['object']
+        return process_sms_object(obj)
+    else:
+        return sms_oops()
+    
+def sms_help():
+    
+    phrase = "Thanks for texting me. It looks like you could use some help. "
+    
+    phrase = phrase + "Try texting the word 'random' to read about a random object from the collection. "
+    
+    phrase = phrase + "You can also text me an object ID number, or an accession number. "
+    
+    phrase = phrase + "To see what Micah might say about this, text 'wwms' and to re-read the help just text 'help' at any time. "
+    
+    return phrase
+    
+def sms_oops():
+    return "Oops, looks like something ain't right. Please text 'help' if you are having trouble. "
+    
+        
+def process_sms_object(obj):
+    
+    object_id = obj['id']
+    medium = obj['medium']
+    title = obj['title']
+    
+    phrase = "Thanks for texting me. "
+    
+    if (title):
 		phrase = phrase + "I'm called " + title + ". "
-		r.sms(phrase)
-		
-	if (medium):
-		medium_phrase = "My medium is " + medium + ". "
-		r.sms(medium_phrase)
-	
-	url = encode(int(id_value))
-	url = "To read more about me, click http://cprhw.tt/o/" + url
-		
-	r.sms(url)
-	
-	return str(r)
+    
+    if (medium):
+        phrase = phrase + "My medium is " + medium + ". "
+        
+    phrase = phrase + "To read more about me, click http://cprhw.tt/o/" + obj['id']
+        
+    return phrase
+    
+def process_body(body):
+    
+    # could be a bunch of stuff .. first check for words
+    if body == 'help':
+        rsp = sms_help()
+    elif body == 'wwms':
+        rsp = wwms()
+    elif body == 'random':
+        rsp = random()    
+    elif is_it_an_int(body):
+        rsp = get_by_object_id(body)
+    else:    
+        rsp = get_by_accession_number(body)
+        
+    return rsp    
 
-@app.route("/end-menu")
-def endmenu():
-	r = twiml.Response()
-	r.say("Thank you for using object phone. Please stay on the line to return to the main menu.")
-	r.redirect(url="/", method="GET")
-	
-	return str(r)	
-		
-			
-		
-	
-def encode(num):
-	alphabet = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'
-	base_count = len(alphabet)
-	encode = ''
-	
-	if (num < 0):
-		return ''
-	
-	while (num >= base_count):	
-		mod = num % base_count
-		encode = alphabet[mod] + encode
-		num = num / base_count
+def is_it_an_int(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
 
-	if (num):
-		encode = alphabet[num] + encode
 
-	return encode
-	
-	
-	
+if __name__ == '__main__':
+
+    if not os.environ.get('CH_API_KEY', False):
+        print "You forgot to set your CH API key as an environment variable"
+        sys.exit()
+            
+    rsp = sms()
+    print rsp
