30e745f5cb205a9fc432a3c215e775aaf8202e6b
Micah Walter <micah@micahwalter.com>
2014-05-12 19:31:29 -0400

    adding cooperhewitt api

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+var cooperhewitt = require('node-cooperhewitt')
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
 app.use(logfmt.requestLogger());
 
+var api_token = process.env.CH_API_KEY;
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+
+	var method = 'cooperhewitt.objects.getRandom';
+	var args = {'access_token': api_token};
+
+	cooperhewitt.call(method, args, function(rsp){   
+	    res.send(rsp);  
+	});	
+	
 });
 
 var port = Number(process.env.PORT || 5000);
